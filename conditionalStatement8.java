93cb8d33a253e0214a5e25e2d4ed3077e0f2d818
Anurag chauhan <Anuragchauhan8510@gmail.com>
2026-02-21 23:55:38 +0530

    loops9

---
+++ conditionalStatement8.java
@@ -0,0 +1,129 @@
+import java.util.Scanner;
+
+public class conditionalStatement8 {
+     public static void main(String args[]) {
+          // int age = 16;
+          // if (age >= 18) {
+          //     System.out.println("Adult");
+          // }
+          // if (age > 13 && age < 18) {
+          //     System.out.println("Teenager");
+          // }
+          // else {
+          //     System.out.println("Not adult");
+          // }
+
+          // int A = 1;
+          // int B = 3;
+          // if (A >= B) {
+          //     System.out.println("A id larger in 2");
+          // }else{
+          //     System.out.println("B id larger in 2");
+          // }
+
+          // Scanner sc = new Scanner(System.in);
+          // int num = sc.nextInt();
+          // if (num % 2 == 0) {
+          //     System.out.println("it is a even Number");
+          // }else{
+          //     System.out.println("it is a odd Number");
+          // }
+          // sc.close();
+
+          // int age = 22;
+          // if (age >= 18) {
+          // System.out.println("Adult");
+          // }
+          // else if (age > 13 && age < 18) {
+          // System.out.println("Teenager");
+          // }
+          // else {
+          // System.out.println("Not adult");
+          // }
+
+          //    Scanner sc = new Scanner(System.in);
+          //    int Income = sc.nextInt();
+          //    int tax;
+          //    if (Income < 500000) {
+          //         tax = 0;   
+          //    }
+          //    else if (Income >= 500000 && Income < 1000000) {
+          //         tax =(int) (Income * 0.2);   
+          //    }
+          //    else {
+          //         tax =(int) (Income * 0.3);
+          //    }
+          //    System.out.println("Your Amount of tax is :" + tax);
+
+          //    sc.close();
+
+          // int A = 1,B = 3, C = 6;
+          // if ((A > B) && (A > C)) {
+          //      System.out.println("A is greater ");
+          // }
+          // else if (B > C) {
+          //      System.out.println("B is greater");
+          // }
+          // else {
+          //      System.out.println("C is greater");
+          // }
+
+          //ternary operator
+          // int num = 4;
+          // String type = ((num % 2) == 0) ? "even" : "odd";
+          // System.out.println(type);
+
+          // Scanner sc = new Scanner(System.in);
+          // int marks = sc.nextInt();
+          // String result = (marks >= 33) ? "Pass" : "Fail";
+          // System.out.println(result);
+          // sc.close();
+
+          //Switch
+          int num = 2;
+          // switch (num) {
+          //      case 3:
+          //           System.out.println("Samosa");
+          //           break;
+          //      case 1:
+          //           System.out.println("Burger");
+          //           break;
+          //      case 2:
+          //           System.out.println("Mangoshake");
+          //           break; 
+          //      default:
+          //           System.out.println("we wakeup");
+          //           break;
+          // }
+
+          Scanner sc = new Scanner(System.in);
+          System.out.println("enter a : ");
+          int a = sc.nextInt();
+          System.out.println("enter operator : ");
+          char operator = sc.next().charAt(0);
+          System.out.println("enter b : ");
+          int b = sc.nextInt();
+
+          switch (operator) {
+               case '+':
+                    System.out.println("Ans : "+(a + b));
+                    break;
+               case '-':
+                    System.out.println("Ans : " + (a - b));
+                    break;
+               case '*':
+                    System.out.println("Ans : " + (a * b));
+                    break;
+               case '/':
+                    System.out.println("Ans : " + (a / b));
+                    break;
+               case '%':
+                    System.out.println("Ans : " + (a % b));
+                    break;
+               default:
+                    System.out.println("wrong operator");
+                    break;
+          }
+          sc.close();
+     }
+}
---
+++ loops9.java
@@ -0,0 +1,114 @@
+import java.util.Scanner;
+
+public class loops9 {
+    public static void main(String args[]) {
+
+        //While loop 
+        // int i=0 ;
+        // while (i < 100) {
+        //     System.out.println("hello world");
+        //     i++;
+        // }
+        // System.out.println("its end");
+
+        // int i = 1;
+        // while (i <= 10) {
+        //     System.out.println(i);
+        //     i++;
+        // 
+
+        // Scanner sc = new Scanner(System.in);
+        // int i = 1;
+        // int n = sc.nextInt();
+        // while (i <= n) {
+        //     System.out.println(i);
+        //     i++;
+        // }
+        // sc.close();
+
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int sum = 0;
+        // int i = 1;
+        // while (i <= n) {
+        //     sum += i;
+        //     i++;
+        // }
+        // System.out.println("sum is " + sum);
+        // sc.close();
+
+        //for loop
+        // for (int i = 1; i <= 10; i++) {
+        //     System.out.println("hello for loop");
+        // }
+
+        // for (int i = 1; i <= 4; i++) {
+        //     System.out.println("* * * *");
+        // }
+
+        //reverese of a number
+        // int n = 10899;
+        // while (n > 0) {
+        //     int l = n % 10;
+        //     System.out.print(l);
+        //     n = n / 10;
+        // }
+        // System.out.println();
+
+        //reverse the given number
+        // int n = 10899;
+        // int rev = 0;
+        // while (n>0) {
+        //     int l = n % 10;
+        //     rev = (rev * 10) + l;
+        //     n = n / 10;
+        // }
+        // System.out.println(rev);
+
+        //do while loop
+        // int i = 1;
+        // do{
+        //     System.out.println("hello do while");
+        //     i++;
+        // } while (i <= 10);
+
+        //break
+        // for (int i = 1; i <= 5; i++) {
+        //     if (i == 3) {
+        //         break;
+        //     }
+        //     System.out.println(i);
+        // }
+        // System.out.println("i am out of loop");
+
+        //break question
+        // Scanner sc = new Scanner(System.in);
+        // do{
+        //     int n = sc.nextInt();
+        //     if(n%10==0){
+        //         break;
+        //     }
+        //     System.out.println(n);
+        // } while (true);
+        // sc.close();
+
+        //continue
+        // for (int i = 1; i <= 5; i++) {
+        //     if (i == 3) {
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        // }
+
+        // Scanner sc = new Scanner(System.in);
+        // do {
+        //     System.out.println("enter your number : ");
+        //     int n = sc.nextInt();
+        // if(n%10==0){
+        // continue;
+        // }
+        // System.out.println("number was : "+n);
+        // } while (true);
+        
+    }
+}
