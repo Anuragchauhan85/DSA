d4371c2494ffa2bc7e9c7a82673296338eda5515
Anurag chauhan <Anuragchauhan8510@gmail.com>
2026-02-25 23:26:45 +0530

    k

--- ArrayP1.java
+++ ArrayP1.java
@@ -1,28 +1,43 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class ArrayP1 {
+
+    public static void Update(int marks[]) {
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = marks[i] + 1;
+        }
+    }
+
     public static void main(String[] args) {
-        int marks[] = new int[50];
+        int marks[] = { 97, 98, 99 };
+        Update(marks);
+       
+        for (int i = 0; i < marks.length; i++) {
+            System.out.print(marks[i] + " ");
+            ;
+        }
+        System.out.println();
+        // int marks[] = new int[50];
 
 
-        System.out.println(marks.length);
-        Scanner sc = new Scanner(System.in);
+        // System.out.println(marks.length);
+        // Scanner sc = new Scanner(System.in);
 
 
-        marks[0] = sc.nextInt();
-        marks[1] = sc.nextInt();
-        marks[2] = sc.nextInt();
+        // marks[0] = sc.nextInt();
+        // marks[1] = sc.nextInt();
+        // marks[2] = sc.nextInt();
 
-        System.out.println("phy : " + marks[0]);
-        System.out.println("chem : " + marks[1]);
-        System.out.println("math : " + marks[2]);
+        // System.out.println("phy : " + marks[0]);
+        // System.out.println("chem : " + marks[1]);
+        // System.out.println("math : " + marks[2]);
 
-        marks[2] = 100;
-        System.out.println("math : " + marks[2]);
+        // marks[2] = 100;
+        // System.out.println("math : " + marks[2]);
 
 
-        int percentage = (marks[0] + marks[1] + marks[2]) / 3;
-        System.out.println("percentage = " + percentage + "%");
-        sc.close();
+        // int percentage = (marks[0] + marks[1] + marks[2]) / 3;
+        // System.out.println("percentage = " + percentage + "%");
+        // sc.close();
     }
 }
