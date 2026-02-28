92058d0660739144e5011fdbd378d03259c641f7
Anurag chauhan <Anuragchauhan8510@gmail.com>
2026-02-28 23:50:59 +0530

    InsertionSort

---
+++ InsertionSort.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class InsertionSort {
+
+    public static void Insertion(Integer arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = curr;
+        }
+    }
+
+    public static void PrintArr(Integer arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Integer arr[] = { 5, 4, 1, 3, 2 };
+        // Insertion(arr);
+        // PrintArr(arr);
+
+        //for inbuilt java sorting
+        Arrays.sort(arr);
+        PrintArr(arr);
+        //for sorting by giving starting to ending index
+        Arrays.sort(arr,0,3);
+        PrintArr(arr);
+        //for reverse sorting 
+        Arrays.sort(arr, Collections.reverseOrder());
+        PrintArr(arr);
+        // for reverse sorting by giving starting to ending index
+        Arrays.sort(arr,0,3, Collections.reverseOrder());
+        PrintArr(arr);
+    }
+}
