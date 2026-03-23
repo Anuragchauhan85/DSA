84dcb8d6035fc96670b8dab00c27cccb6c36ad3b
Anurag chauhan <Anuragchauhan8510@gmail.com>
2026-03-23 23:34:23 +0530

    Strings

---
+++ Strings.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Strings {
+
+    public static void main(String[] args) {
+       char arr[]={'a','b','c','d'};
+       String str="abcd";
+       String str2=new String("xyz");
+       //java strigs are immutable
+
+       Scanner sc=new Scanner(System.in);
+       String name;
+       //    name = sc.next(); //sc.next only take one word sc.nextLine take full line.
+       name = sc.nextLine();
+       System.err.println(name);
+    }
+}
