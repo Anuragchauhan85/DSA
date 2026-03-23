e7bb0b693b8860e7b8538dfb5764581e42c599be
Anurag chauhan <Anuragchauhan8510@gmail.com>
2026-03-23 23:47:39 +0530

    concatination

--- Strings.java
+++ Strings.java
@@ -3,15 +3,27 @@ import java.util.Scanner;
 public class Strings {
 
     public static void main(String[] args) {
-       char arr[]={'a','b','c','d'};
-       String str="abcd";
-       String str2=new String("xyz");
-       //java strigs are immutable
+    //    char arr[]={'a','b','c','d'};
+    //    String str="abcd";
+    //    String str2=new String("xyz");
+    //    //java strigs are immutable
 
-       Scanner sc=new Scanner(System.in);
-       String name;
-       //    name = sc.next(); //sc.next only take one word sc.nextLine take full line.
-       name = sc.nextLine();
-       System.err.println(name);
+    //    Scanner sc=new Scanner(System.in);
+    //    String name;
+    //    //    name = sc.next(); //sc.next only take one word sc.nextLine take full line.
+    //    name = sc.nextLine();
+    //    System.err.println(name);
+
+
+    // String fullName = "Tony Stark";
+    // System.out.println(fullName.length());
+
+
+    //concatnation
+    String firstname = "Anurag";
+    String lastname = "chauhan";
+    String fullname = firstname + " " + lastname;
+    System.out.println(fullname);
     }
 }
+ 
