9119281837280e7a81674fb61899385570728648
Anurag chauhan <Anuragchauhan8510@gmail.com>
2026-04-16 21:42:41 +0530

    Bitmanipulation

--- BitmanipulationAnd.java
+++ BitmanipulationAnd.java
@@ -3,6 +3,8 @@ public class BitmanipulationAnd {
         // System.out.println(5 & 6);   and
         // System.out.println(5 | 6);   or
         // System.out.println(5 ^ 6);   xor
-        System.out.println(5 | 6); 
+        // System.out.println((~0));    1's compliments
+        // System.out.println((5<<2));   binary left shift a<<b =a*2^b   
+        //System.out.println((5>>2));    binary right shift a<<b =a*2^b
     }
 }
