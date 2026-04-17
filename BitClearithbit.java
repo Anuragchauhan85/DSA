02bae07bfa638a885647aec1ceed351c3a030317
Anurag chauhan <Anuragchauhan8510@gmail.com>
2026-04-17 22:41:34 +0530

    clear ith bit

---
+++ BitClearithbit.java
@@ -0,0 +1,10 @@
+public class BitClearithbit {
+
+    public static int ClearIthBit(int n , int i) {
+        int bitmask = ~(1 << i);
+        return n & bitmask;
+    }
+    public static void main(String[] args) {
+        System.out.println(ClearIthBit(10, 1));
+    }
+}
