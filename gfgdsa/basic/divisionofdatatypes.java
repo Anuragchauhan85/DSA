0e372e0b3d6218d633e3e9b48eb724e9ebe03c4d
Anurag <anuragchauhan8510@gmail.com>
2026-09-11 23:32:58 +0530

    divisionofdatatypes

---
+++ gfgdsa/basic/divisionofdatatypes.java
@@ -0,0 +1,6 @@
+public class divisionofdatatypes{
+   public static void main(String[] args) {
+    double x = 5/2.0;                         //int and int is int not give ans in double       double and other is double
+    System.out.println(x);
+   }
+}
