7f563db1266ca042a436d65d52e2b128ad34755c
Anurag <anuragchauhan8510@gmail.com>
2026-09-11 23:31:58 +0530

    airthmetic operators in javva

---
+++ gfgdsa/basic/airthmetic.java
@@ -0,0 +1,9 @@
+public class airthmetic {
+    public static void main(String[] args) {
+        double x = 3, y = 5;
+        System.out.println(x+y);
+        System.err.println(x-y);
+        System.out.println(x*y);
+        System.out.println(y/x);
+    }
+}
