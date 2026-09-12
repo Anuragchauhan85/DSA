f314b66416845aaa5e44bf9612b35fa6f63c2252
Anurag <anuragchauhan8510@gmail.com>
2026-09-12 23:53:23 +0530

    increamentdecrement

--- gfgdsa/basic/increamentdecrement.java
+++ gfgdsa/basic/increamentdecrement.java
@@ -1,3 +1,19 @@
 public class increamentdecrement {
-    
+    public static void main(String[] args) {
+        int x=10;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int y=10;
+        System.out.println(++y);
+        System.out.println(y);
+
+        int z=10;
+        System.out.println(z--);
+        System.out.println(z);
+
+        int v=10;
+        System.out.println(--v);
+        System.out.println(v);
+    }
 }
