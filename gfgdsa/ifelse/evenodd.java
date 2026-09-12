732c07e39ff56e2c94f136a38ac9f12335d2886e
Anurag <anuragchauhan8510@gmail.com>
2026-09-12 23:53:44 +0530

    evenodd

---
+++ gfgdsa/ifelse/evenodd.java
@@ -0,0 +1,21 @@
+package gfgdsa.ifelse;
+
+
+import java.util.Scanner;
+
+public class evenodd {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("enter Number : ");
+
+        int n = sc.nextInt();
+
+        if (n%2==0){
+            System.out.println("it is a even number");
+        }
+        else{
+            System.out.println("it is a odd number");
+        }
+        sc.close();
+    }
+}
