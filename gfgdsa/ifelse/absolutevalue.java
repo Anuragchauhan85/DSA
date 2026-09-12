5da10fa653f897240db92a1d3c3831e7be6d5e10
Anurag <anuragchauhan8510@gmail.com>
2026-09-12 23:53:32 +0530

    absolutevalue

---
+++ gfgdsa/ifelse/absolutevalue.java
@@ -0,0 +1,17 @@
+package gfgdsa.ifelse;
+
+import java.util.Scanner;
+
+public class absolutevalue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n >= 0) {
+            System.out.println(n);
+        } else {
+            System.out.println(-n);
+        }
+        sc.close();
+    }
+}
