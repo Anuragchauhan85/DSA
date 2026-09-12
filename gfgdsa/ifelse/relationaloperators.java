53c4846ad587a7181c3dc135b322df21cbbc7a58
Anurag <anuragchauhan8510@gmail.com>
2026-09-12 23:54:00 +0530

    relationaloperators#

---
+++ gfgdsa/ifelse/relationaloperators.java
@@ -0,0 +1,15 @@
+package gfgdsa.ifelse;
+
+public class relationaloperators {
+    public static void main(String[] args) {
+        System.out.println(5>3);
+        System.out.println(5<3);
+        System.out.println(5>=3);
+        System.out.println(5<=3);
+        System.out.println(5==3);
+        System.out.println(5!=3);
+
+        boolean x= (5>3);
+        System.out.println(x);
+    }
+}
