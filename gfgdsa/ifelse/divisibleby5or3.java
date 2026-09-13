7a2b6c4fbf28132dde2059104121c71b5532362a
Anurag <anuragchauhan8510@gmail.com>
2026-09-14 00:02:03 +0530

    divisibleby5or3

---
+++ gfgdsa/ifelse/divisibleby5or3.java
@@ -0,0 +1,16 @@
+package gfgdsa.ifelse;
+
+import java.util.Scanner;
+
+public class divisibleby5or3 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        int n = sc.nextInt();
+        if(n%5==0 || n%3==0){
+           System.out.println("it is divisible by 5 or 3");
+        }else{
+            System.out.println("it is not divisible by 5 or 3");
+        }
+    }
+}
