0b6bfbaad19a671cb1e3ccfc7492242df1c73265
Anurag <anuragchauhan8510@gmail.com>
2026-09-16 23:50:20 +0530

    ternaryinvariable

---
+++ gfgdsa/ifelse/ternaryinvariable.java
@@ -0,0 +1,17 @@
+package gfgdsa.ifelse;
+
+import java.util.Scanner;
+
+public class ternaryinvariable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter n : ");
+        int n = sc.nextInt();
+
+        int prasun = (n>=0)? 100 : 0 ;
+
+        System.out.println(prasun);
+        sc.close();
+    }
+}
