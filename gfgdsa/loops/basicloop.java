e0dfdadbc7e16b7069015795c4ee3c6fc43e7154
Anurag <anuragchauhan8510@gmail.com>
2026-09-18 23:53:37 +0530

    basicloop

---
+++ gfgdsa/loops/basicloop.java
@@ -0,0 +1,14 @@
+package gfgdsa.loops;
+
+public class basicloop {
+    public static void main(String[] args) {
+        // for (int i=1; i<=3; i++){
+        //     System.out.println("Anurag");
+        // };
+
+
+        for (int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
