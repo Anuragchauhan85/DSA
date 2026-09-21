4786118b5607d94525d247cd4ddd9199617a2b0b
Anurag <anuragchauhan8510@gmail.com>
2026-09-21 23:51:29 +0530

    printdecresingseries

--- gfgdsa/loops/printdecresingseries.java
+++ gfgdsa/loops/printdecresingseries.java
@@ -2,9 +2,9 @@ package gfgdsa.loops;
 //print series 99,95,91,87,83.... upto positive numbers
 public class printdecresingseries {
     public static void main(String[] args) {
-        int j=99;
-        for( int i=j;j>=0; j-=4){
-            System.out.println(i);
+        
+        for( int i=99;i>0; i-=4){
+            System.out.println(i);    
         }
     }
 }
